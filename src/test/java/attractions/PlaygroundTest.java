e4e5e721f1e1125d882029b62ed7a67326a1387a
madgelackie <marionmlackie@gmail.com>
2021-11-22 16:57:25 +0000

    playground visit security functioning

--- src/test/java/attractions/PlaygroundTest.java
+++ src/test/java/attractions/PlaygroundTest.java
@@ -2,15 +2,20 @@ package attractions;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
 
 public class PlaygroundTest {
     Playground playground;
+    Visitor visitor1;
+    Visitor visitor2;
 
     @Before
     public void setUp() throws Exception {
         playground = new Playground("Fun Zone", 7);
+        visitor1 = new Visitor(9, 130, 30);
+        visitor2 = new Visitor(16, 145, 25);
     }
 
     @Test
@@ -27,4 +32,15 @@ public class PlaygroundTest {
     public void hasVisitCount() {
         assertEquals(0, playground.getVisitCount());
     }
+
+    @Test
+    public void canVisit(){
+        assertEquals(true, playground.isAllowedTo(visitor1));
+    }
+
+    @Test
+    public void cantVisit(){
+        assertEquals(false, playground.isAllowedTo(visitor2));
+    }
+
 }
